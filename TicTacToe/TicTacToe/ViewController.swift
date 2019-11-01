2c786135f3c01a544f84694c443e25930594fbb5
Tsering Lama <tseringlama@pursuit.org>
2019-11-01 11:54:53 -0400

    added grid, 9 buttons, reset game and label

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+    
+    @IBOutlet var gameButtons: [GameButton]!
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var resetGame: UIButton!
+    @IBOutlet weak var gridImage: UIImageView!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
+    
+    turnLabel.text = "Welcome to tic-tack-toe"
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+    
+    
+    @IBAction func resetGame(_ sender: UIButton) {
+        viewDidLoad()
+        
+    }
+    
+  
+    @IBAction func buttonPressed(_ sender: GameButton) {
+        
+    }
+    
 
 }
 
