574d6d0059cff2de21df3813e9a92fc946516d18
Tsering Lama <tseringlama@pursuit.org>
2019-11-03 19:08:24 -0500

    got the game to alternate between playe 1 and 2

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -11,30 +11,45 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var gameButtons: [GameButton]!
-    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var resetGame: UIButton!
     @IBOutlet weak var gridImage: UIImageView!
     
+    @IBOutlet weak var whosTurn: UILabel!
+    
+    var player = 1
+    
     override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    turnLabel.text = "Welcome to tic-tack-toe"
-    
-    
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome to tic-tack-toe"
+        whosTurn.text = "Player 1, its your turn"
+        //        sender.setBackgroundImage(UIImage(named: "omark"), for: .normal)
+        
+    }
     
     
     @IBAction func resetGame(_ sender: UIButton) {
+        for button in gameButtons {
+            button.isHidden = true
+        }
         viewDidLoad()
-        
     }
     
-  
     @IBAction func buttonPressed(_ sender: GameButton) {
         
+        if player == 1 {
+            whosTurn.text = "Player 2, its your turn"
+            sender.setBackgroundImage(UIImage(named: "omark"), for: .normal)
+            player = 2
+        } else  {
+            whosTurn.text = "Player 1, its your turn"
+            sender.setBackgroundImage(UIImage(named: "xmark"), for: .normal)
+            player = 1
+        }
+        
+        
     }
     
-
+    
 }
 
